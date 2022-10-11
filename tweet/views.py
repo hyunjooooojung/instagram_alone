c6a2b2cdb075400e80e5e15a3cbaf83d61c89cfe
hyunjoojung <hyunjoo3107@gmail.com>
2022-10-11 22:06:59 +0900

    mainpage 수정/ views.py에 tweet 함수 추가

--- tweet/views.py
+++ tweet/views.py
@@ -16,3 +16,24 @@ def home(request):
         return redirect('/home')
     else:
         return redirect('/sign-in')
+    
+
+def tweet(request):
+    if request.method == "GET":
+        # 사용자가 로그인 되어있는지 확인하기
+        user = request.user.is_authenticated
+        
+        if user: # 로그인 되어있는 사용자라면 
+            all_tweet = TweetModel.objects.all().order_by('-created_at')
+            all_comment = TweetComment.objects.all().order_by('-created_at')
+            return render(request, 'tweet/home.html', {'tweet': all_tweet, 'comment': all_comment})
+        else: # 로그인 되어있지 않다면
+            return redirect('/sign-in')
+        
+    elif request.method == "POST":
+        user = request.user
+        my_tweet = TweetModel()  # 글쓰기 모델 가져오기
+        my_tweet.author = user  # 모델에 사용자 저장
+        my_tweet.content = request.POST.get('my-content', '')  # 모델에 글 저장
+        my_tweet.save()
+        return redirect('/tweet')
